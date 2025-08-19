e11a030bfb5d4007015cbf0a205a5afac9dc8767
Braian Martínez <b2414nm@gmail.com>
2025-08-19 18:21:21 -0300

    mejora estetica y avances con las demás secciones

--- script.js
+++ script.js
@@ -5,8 +5,34 @@ document.querySelectorAll('.dot').forEach(dot => {
     });
 });
 
-document.querySelectorAll('.dot').forEach((dot, index) => {
+document.querySelectorAll('.dot0').forEach((dot, index) => {
     dot.addEventListener('click', () => {
-        window.location.href = `portafolio_web${/*index + */1}.html`;
+        window.location.href = `portafolio_web.html`;
     });
+});
+
+document.querySelectorAll('.dot1').forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        window.location.href = `portafolio_web1.html`;
+    });
+});
+
+document.querySelectorAll('.dot2').forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        window.location.href = `portafolio_web2.html`;
+    });
+});
+
+document.querySelectorAll('.dot3').forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        window.location.href = `portafolio_web3.html`;
+    });
+});
+
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-bar');
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active'); 
+  nav.classList.toggle('open');     
 });
